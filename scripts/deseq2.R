bf7f4b93e85a5fbf65f8d42822c55011ab7db348
HannahVMeyer <Hannah.v.meyer@gmail.com>
2020-11-22 15:53:11 -0500

    Simplify input

--- scripts/deseq2.R
+++ scripts/deseq2.R
@@ -8,6 +8,41 @@ sink(log, type="message")
 library("DESeq2")
 library("tidyverse")
 
+
+#################
+## functions ####
+#################
+genes_de <- function(deset, thrP=0.05, thrLog2FC=log2(1.5),
+                     direction=c('up', 'down', 'any')) {
+    tmp <- deset %>%
+        as.data.frame %>%
+        rownames_to_column(var="gene_id")
+    if (direction == "up") {
+        tmp <- tmp %>%
+            dplyr::filter(padj < thrP,
+                          log2FoldChange > thrLog2FC)
+    } else if (direction == "down") {
+        tmp <- tmp %>%
+            dplyr::filter(padj < thrP,
+                          log2FoldChange < thrLog2FC)
+    } else if (direction == "any") {
+        tmp <- tmp %>%
+            dplyr::filter(padj < thrP)
+    } else {
+        stop(direction, "is not a valid option for direction")
+    }
+    tmp
+}
+
+
+save_up_down <- function(res, snakemake) {
+    up  <- genes_de(res, direction="up")
+    down  <- genes_de(res, direction="down")
+    write_csv(up, snakemake@output[['up']])
+    write_csv(down, snakemake@output[['down']])
+    return(list(up=up$gene_id, down=down$gene_id))
+}
+
 ############
 ## data ####
 ############
@@ -20,12 +55,11 @@ dds <- readRDS(snakemake@input[["dds"]])
 
 ## 1. Model fit ####
 # Generate named coefficients need for apeglm lfcShrink
-elements <- snakemake@params[["contrast"]]
-comparison <- paste(elements[1], "vs", elements[2], sep="_")
-coef <- paste("condition", elements[1], "vs", elements[2], sep="_")
+comparison <- snakemake@wildcards[["contrast"]]
+coef <- paste("condition", comparison, sep="_")
 
 # Relevel for reference to second element in contrasts
-dds$condition <- relevel(dds$condition, elements[2])
+dds$condition <- relevel(dds$condition, "control")
 dds <- nbinomWaldTest(dds)
 
 ## 2. Process results ####
